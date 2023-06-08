fc1a5a0fb297e9cd0513bf111ec37ecae1749d6e
MoloG83 <55248942+MoloG83@users.noreply.github.com>
2023-06-08 16:46:34 +0100

    small changes

--- SailView/wwwroot/scripts/pdfgenerator.js
+++
@@ -1,8 +0,0 @@
-window.generatePDF = async () => {
-    var element = document.body;
-    var canvas = await html2canvas(element);
-    var imgData = canvas.toDataURL('image/png');
-    var pdf = new jsPDF();
-    pdf.addImage(imgData, 'PNG', 0, 0);
-    pdf.save("download.pdf");
-};
